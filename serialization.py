47222b8eb158a000e3344956cec80de72da5b638
TankManBeta <1148099884@qq.com>
2021-12-09 16:04:21 +0800

    serialization

---
+++ serialization.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author 坦克手贝塔
+    @Date 2021/12/8 16:20
+"""
+import pickle
+
+# 序列化
+with open('./data.dat', 'wb') as f:
+    a = "1"
+    b = 1
+    c =[1]
+
+    pickle.dump(a, f)
+    pickle.dump(b, f)
+    pickle.dump(c, f)
+
+# 反序列化
+with open('./data.dat', 'rb') as f:
+    a = pickle.load(f)
+    b = pickle.load(f)
+    c = pickle.load(f)
+
+    print(a)
+    print(b)
+    print(c)
