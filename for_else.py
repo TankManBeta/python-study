7f9cde44e571653c2a3bb16c4fb0901eb567d264
tankmanbeta <1148099884@qq.com>
2022-02-24 18:55:20 +0800

    new knowledge

---
+++ for_else.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author 坦克手贝塔
+    @Date 2022/2/24 18:51
+"""
+for i in range(10):
+    if i == 5:
+        print(f"Find {i}")
+        break
+else:
+    print("Not find")
