50c94339f1d77441741175a5fda5792735c67237
tankmanbeta <1148099884@qq.com>
2023-01-18 17:30:14 +0800

    new knowledge

---
+++ Net_related/socket_UDP.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author 坦克手贝塔
+    @Date 2023/1/17 20:44
+"""
+from socket import *
+
+s = socket(AF_INET, SOCK_DGRAM)
+# 绑定一个接受消息使用的端口
+s.bind(('', 9988))
+addr = ("192.168.100.107", 9999)
+data = input("请输入要发送的消息：")
+s.sendto(data.encode("gb2312"), addr)
+# 本次接受的最大字节数
+re_data = s.recvfrom(1024)
+print(re_data)
+s.close()
